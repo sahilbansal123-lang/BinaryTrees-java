38594dcc00e45f0a6291eff7aca5f630072ec021
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-04-02 21:37:08 +0530

    binary tree

---
+++ src/BinaryTree.java
@@ -0,0 +1,16 @@
+
+
+//     Binary trees: are a type of data structure commonly used in computer science and programming. A binary tree
+//                   is a tree data structure in which each node has at most two children, referred to as the left child
+//                   and the right child. The nodes themselves can contain data, such as a number, string, or object.
+//
+//                   The topmost node of the tree is called the root, and it is the only node that does not have a parent.
+//                   The children of a node can also be nodes that have their own children, creating a hierarchy that can be used
+//                   to represent relationships between data.
+
+
+public class BinaryTree {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
