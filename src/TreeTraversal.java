8775d70838f26c96e0e1f61127a97392b23281ae
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-04-06 12:13:46 +0530

    binary tree

--- src/TreeTraversal.java
+++ src/TreeTraversal.java
@@ -170,17 +170,99 @@ public class TreeTraversal {
         return new TreeInfo(myHeight, myDiam);
     }
 
-
+//  Ques5: Subtree of a tree
     //  Time complexity: O()
+public static boolean isSubtree(Node tree, Node subTree) {
+    if (subTree == null) {
+        // An empty tree is always a subtree
+        return true;
+    }
+
+    if (tree == null) {
+        // We've reached the end of the main tree
+        return false;
+    }
+
+    // Check if the current nodes match
+    if (tree.data == subTree.data && isIdentical(tree, subTree)) {
+        return true;
+    }
+
+    // Check if the subtree is in the left or right subtree of the current node
+    return isSubtree(tree.left, subTree) || isSubtree(tree.right, subTree);
+}
+
+    public static boolean isIdentical(Node tree1, Node tree2) {
+        if (tree1 == null && tree2 == null) {
+            return true;
+        }
+
+        if (tree1 == null || tree2 == null) {
+            return false;
+        }
+
+        if (tree1.data != tree2.data) {
+            return false;
+        }
+
+        return isIdentical(tree1.left, tree2.left) && isIdentical(tree1.right, tree2.right);
+    }
+
+//  Ques6: sum of nodes at Kth level\
+    public static int sumAtKth(Node root, int k){
+        if(root == null) {
+            return 0;
+        }
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        q.add(null);
+        int level = 0;
+        int sum = 0;
+        while(!q.isEmpty()){
+            Node currNode = q.remove();
+            if (currNode == null) {
+                level++;
+                if(q.isEmpty()){
+                    break;
+                } else {
+                    q.add(null);
+                }
+            } else {
+                if (level == k ){
+                    sum = sum + currNode.data;
+                }
+                if (currNode.left!=null) {
+                    q.add(currNode.left);
+                }
+                if (currNode.right!=null) {
+                    q.add(currNode.right);
+                }
+            }
+        }
+        return sum;
+    }
+
+
+
     public static void main(String[] args) {
-        int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+        int nodes[] = {1, 2, 4, -1, -1, 7, -1, -1, 3, -1, 6, -1, -1, };
+//        int nodes[] = {2, 4, -1, -1, 5};
         BinaryTree tree = new BinaryTree();
+//        BinaryTree subTree = new BinaryTree();
         Node root = tree.buildTree(nodes);
+//        Node subRoot = subTree.buildTree(node1);
+
+//        System.out.println(isSubtree(root, subRoot));
+
         levelOrder(root);
         System.out.println(count(root));
         System.out.println(sum(root));
         System.out.println(height(root));
         System.out.println(diameter(root));
-        System.out.println(diameter2(root));
+        TreeInfo result = diameter2(root);
+        System.out.println("Height: " + result.height);
+        System.out.println("Diameter: " + result.diam);
+        System.out.println(sumAtKth(root, 2));
+
     }
 }
