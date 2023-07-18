24483ecfd849ebf9aa7f68d721dd8a355db69454
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-07-18 23:51:08 +0530

    binary tree

--- src/BuildBinaryTreeyt.java
+++ src/BuildBinaryTreeyt.java
@@ -41,6 +41,7 @@ public class BuildBinaryTreeyt {
     public static void main(String[] args) {
         int[] nodes = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
         Node root = BinaryTree.buildTree(nodes);
+
         System.out.println(root.data);
     }
 
