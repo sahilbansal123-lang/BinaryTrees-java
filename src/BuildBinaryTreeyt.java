22c7b0fdcbfc004df62205d68bf719902c59d38c
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-07-18 23:50:22 +0530

    binary tree

--- src/BuildBinaryTreeyt.java
+++ src/BuildBinaryTreeyt.java
@@ -10,37 +10,37 @@
 
 public class BuildBinaryTreeyt {
 
-    static class Node{
-        int data;
-        Node left;
-        Node right;
-        Node(int data){
-            this.data = data;
-            this.left = null;
-            this.right = null;
-        }
-    }
+   static class Node {
+       Node right;
+       Node left; 
+       int data;
+       
+       public Node(int data) {
+           this.data = data;
+           this.right = null;
+           this.left = null;
+       }
+   }
+   
+   static class BinaryTree {
+       static int idx = -1;
+       public static Node buildTree(int[] nodes) {
+           idx++;
+           if (nodes[idx] == -1) {
+               return null;
+           }
+           Node newNode = new Node(nodes[idx]);
+           newNode.left = buildTree(nodes);
+           newNode.right = buildTree(nodes);
 
-    static class BinaryTree {
-        static int idx = -1;
-        public static Node buildTree(int nodes[]) {
-            idx++;
-            if(nodes[idx] == -1) {
-                return null;
-            }
-            Node newNode = new Node(nodes[idx]);
-            newNode.left = buildTree(nodes);
-            newNode.right = buildTree(nodes);
-            return newNode;
-        }
-    }
+           return newNode;
+       }
+   }
 
 
-    public static void main(String args[]) {
-        int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
-        BinaryTree tree = new BinaryTree();
-
-        Node root = tree.buildTree(nodes);
+    public static void main(String[] args) {
+        int[] nodes = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
+        Node root = BinaryTree.buildTree(nodes);
         System.out.println(root.data);
     }
 
