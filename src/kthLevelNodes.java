9bae27e7f8d6fed3f39de4ae6fb2d65235f0cab4
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-07-20 22:24:13 +0530

    binary tree

--- src/kthLevelNodes.java
+++ src/kthLevelNodes.java
@@ -1,6 +1,17 @@
+
 public class kthLevelNodes {
 
-    public static void kthLevel() {
+    public static void kthLevel(DiameterOfTree.Node root, int level, int k) {
+        if (level == k) {
+            System.out.println(root.data + " ");
+            return;
+        }
+        if (root == null) {
+            return;
+        }
+
+        kthLevel(root.left, level+1, k);
+        kthLevel(root.right, level+1, k);
 
     }
 
