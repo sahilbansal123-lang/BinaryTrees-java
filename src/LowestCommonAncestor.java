dbf4a913f5857b122e65d297c9f4c6f931d7c3f3
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-07-20 23:29:22 +0530

    binary tree

--- src/LowestCommonAncestor.java
+++ src/LowestCommonAncestor.java
@@ -3,8 +3,8 @@ import java.util.ArrayList;
 public class LowestCommonAncestor {
 
     static class Node {
-        BuildBinaryTreeyt.Node right;
-        BuildBinaryTreeyt.Node left;
+        Node right;
+        Node left;
         int data;
 
         public Node(int data) {
@@ -13,7 +13,25 @@ public class LowestCommonAncestor {
             this.left = null;
         }
     }
-    public static void getPath(Node root, int n1, ArrayList<Node> path1) {
+    public static boolean getPath(Node root, int n, ArrayList<Node> path) {
+        if (root == null) {
+            return false;
+        }
+
+        path.add(root);
+        if (root.data == n) {
+            return true;
+        }
+
+        boolean foundLeft = getPath(root.left, n, path);
+        boolean foundRight = getPath(root.right, n, path);
+
+        if (foundLeft || foundRight) {
+            return true;
+        }
+
+        path.remove(path.size()-1);
+        return false;
     }
     public static Node lowectCommonAncestor(Node root, int n1, int n2) {
         ArrayList<Node> path1 = new ArrayList<>();
