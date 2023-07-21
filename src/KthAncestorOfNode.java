524f1d7df6ed1601b73d7d4291473622febb42f5
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-07-21 11:03:48 +0530

    binary tree

---
+++ src/KthAncestorOfNode.java
@@ -0,0 +1,22 @@
+public class KthAncestorOfNode {
+
+    public static class Node {
+        Node left;
+        Node right;
+        int data;
+
+        public Node(int data) {
+            this.left = null;
+            this.right = null;
+            this.data = data;
+        }
+    }
+
+    public static void kthAncestor(Node root, int n, int k) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
