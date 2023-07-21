f4644089f04d2dc9c0c81d319a90122cb1586bbc
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-07-21 11:08:42 +0530

    binary tree

--- src/KthAncestorOfNode.java
+++ src/KthAncestorOfNode.java
@@ -12,8 +12,22 @@ public class KthAncestorOfNode {
         }
     }
 
-    public static void kthAncestor(Node root, int n, int k) {
+    public static int kthAncestor(Node root, int n, int k) {
+        if (root.data == n) {
+            return 0;
+        }
+        int leftDist = kthAncestor(root.left, n, k);
+        int rightDist = kthAncestor(root.right, n, k);
 
+        if (leftDist == -1 || rightDist == -1) {
+            return -1
+        }
+        int max = Math.max(leftDist, rightDist);
+        if (max + 1 == k) {
+            System.out.println(root.data);
+        }
+
+        return max+1;
     }
 
     public static void main(String[] args) {
