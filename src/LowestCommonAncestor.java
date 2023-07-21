2060b72c6bfe5480cb2cb23f67d04aeb2803c5a1
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-07-21 09:59:59 +0530

    binary tree

--- src/LowestCommonAncestor.java
+++ src/LowestCommonAncestor.java
@@ -60,6 +60,26 @@ public class LowestCommonAncestor {
 
                                         //  APPROACH 2
 
+    public static Node lca2(Node root, int n1, int n2) {
+        if (root == null) {
+            return null;
+        }
+        if (root.data == n1 || root.data == n2) {
+            return root;
+        }
+
+        Node leftLca = lca2(root.left, n1, n2);
+        Node rightLca = lca2(root.right, n1, n2);
+
+        if (leftLca == null) {
+            return rightLca;
+        }
+        if (rightLca == null) {
+            return leftLca;
+        }
+
+        return root;
+    }
 
 
     public static void main(String[] args) {
