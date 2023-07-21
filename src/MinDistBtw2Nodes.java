a0044b53a8e97baecfb0143dbe71788d9e7aed41
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-07-21 10:41:53 +0530

    binary tree

---
+++ src/MinDistBtw2Nodes.java
@@ -0,0 +1,77 @@
+
+public class MinDistBtw2Nodes {
+
+    public static class Node {
+        int data;
+        Node right;
+        Node left;
+
+        public Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static Node lca(Node root, int n1, int n2) {
+        if (root == null) {
+            return null;
+        }
+        if (root.data == n1 || root.data == n2) {
+            return root;
+        }
+
+        Node leftLca = lca(root.left, n1, n2);
+        Node rightLca = lca(root.right, n1, n2);
+
+        if (leftLca == null) {
+            return rightLca;
+        }
+        if (rightLca == null) {
+            return leftLca;
+        }
+        return root;
+    }
+    public static int dist(Node root, int n) {
+        if (root == null) {
+            return -1;
+        }
+        if (root.data == n) {
+            return 0;
+        }
+
+        int leftDist = dist(root.left, n);
+        int rightDist = dist(root.right, n);
+
+        if (leftDist == -1 && rightDist == -1) {
+            return -1;
+        } else if (leftDist == -1) {
+            return rightDist + 1;
+        } else {
+            return leftDist + 1;
+        }
+    }
+
+    public static int minDist(Node root, int n1, int n2) {
+
+        Node Lca = lca(root, n1, n2);
+
+        int dist1 = dist(Lca, n1);
+        int dist2 = dist(Lca, n2);
+
+        return dist1 + dist2;
+    }
+
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        System.out.println(minDist(root, 4, 6));
+
+    }
+}
