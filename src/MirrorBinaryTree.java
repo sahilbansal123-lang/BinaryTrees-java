16fdd0c1cdff5ea68033d2e0ce58ca128df11108
SAHIL BANSAL <BANSALSAHIL9053@GMAIL.COM>
2023-07-21 13:23:39 +0530

    binary tree

--- src/MirrorBinaryTree.java
+++ src/MirrorBinaryTree.java
@@ -1,12 +1,32 @@
 public class MirrorBinaryTree {
 
-    public static CheckUnivalued.Node mirrorBt(CheckUnivalued.Node root) {
+    static class Node {
+        Node right;
+        Node left;
+        int data;
+
+        public Node(int data) {
+            this.data = data;
+            this.right = null;
+            this.left = null;
+        }
+    }
+
+    static class Mirror {
+        Node roots;
+
+        public void Mirror() {
+            roots = mirrorBt(roots);
+        }
+    }
+
+    public static Node mirrorBt(Node root) {
         if (root == null) {
             return root;
         }
 
-        CheckUnivalued.Node left = mirrorBt(root.left);
-        CheckUnivalued.Node right = mirrorBt(root.right);
+        Node left = mirrorBt(root.left);
+        Node right = mirrorBt(root.right);
 
         root.left = right;
         root.right = left;
@@ -14,7 +34,28 @@ public class MirrorBinaryTree {
         return root;
     }
 
-    public static void main(String[] args) {
+    public static void preOrder(Node root) {
+        if (root == null) {
+            return;
+        }
 
+        preOrder(root.left);
+        System.out.print(root.data + " ");
+        preOrder(root.right);
+     }
+
+    public static void main(String[] args) {
+        Node root = new Node(1);
+        root.left = new Node(2);
+        root.right = new Node(3);
+        root.left.left = new Node(4);
+        root.left.right = new Node(5);
+        root.right.left = new Node(6);
+        root.right.right = new Node(7);
+
+        preOrder(root);
+        mirrorBt(root);
+        System.out.println();
+        preOrder(root);
     }
 }
